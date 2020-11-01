934da7febab36beb3f51e0d7876349c98d855136
a-makhneva <70952443+a-makhneva@users.noreply.github.com>
2020-11-01 20:00:46 +0300

    Update sudoku.py

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -5,7 +5,6 @@ import typing as tp
 
 T = tp.TypeVar("T")
 
-
 def create_grid(puzzle: str) -> List[List[str]]:
     digits = [c for c in puzzle if c in "123456789."]
     grid = group(digits, 9)
@@ -107,7 +106,6 @@ def find_empty_positions(grid: List[List[str]]) -> Tuple[int, int]:
             if myrow[j] == ".":
                 x = (i, j)
                 return x
-
     return x
 
 
